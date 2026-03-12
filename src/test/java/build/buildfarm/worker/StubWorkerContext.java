29cfd1ebaf862d07a626afc806cb92b4606d2a3f
Timothy Blamires <tblamires@lucidchart.com>
2026-03-12 13:37:43 -0600

    Don't enforce alwaysUseAsNobody for actions running on persistent workers
    Buildfarm does not properly set ownership for persistent worker exec
    roots, so alwaysUseAsNobody is skipped for persistent workers until
    further work is done to support this.

--- src/test/java/build/buildfarm/worker/StubWorkerContext.java
+++ src/test/java/build/buildfarm/worker/StubWorkerContext.java
@@ -214,7 +214,8 @@ class StubWorkerContext implements WorkerContext {
       UserPrincipal owner,
       ImmutableList.Builder<String> arguments,
       Command command,
-      Path workingDirectory) {
+      Path workingDirectory,
+      boolean runsOnPersistentWorker) {
     throw new UnsupportedOperationException();
   }
 
